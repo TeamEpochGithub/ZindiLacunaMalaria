0e1a11aedb596f3bb044d7c011061bb320ae907e
MarJarAI <m.w.jarosz@student.tudelft.nl>
2024-11-12 11:39:02 +0100

    add bypass for detr

--- training/detr/train_detr_models.py
+++ training/detr/train_detr_models.py
@@ -2,6 +2,7 @@ import yaml
 from .core.model import get_model, get_processor
 from .core.train import train
 from pprint import pprint
+import torch
 
 def get_trained_detr_models(config_files, dataset_path) -> list:
     trained_detr_models = []
@@ -9,7 +10,10 @@ def get_trained_detr_models(config_files, dataset_path) -> list:
         config_dict = dict(yaml.safe_load(open(config_file)))
         model = get_model(config_dict.pop('model'))
         processor = get_processor(config_dict.pop('processor'))
-        device = config_dict['device']
-        train(config_dict, model, processor, data_dir=dataset_path, device=device)
+        # device = config_dict['device']
+        # train(config_dict, model, processor, data_dir=dataset_path, device=device)
+        
+        # Load pretrained weights instead of training
+        model.load_state_dict(torch.load('/path/to/detr/weights.pth')) # MAKE SURE THE LOADED MODEL WAS TRAINED WITH THE SAME CONFIG
         trained_detr_models.append(model)
     return trained_detr_models
